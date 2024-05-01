7dfb4023045908d63017e361bae902e58bf6d7d1
Taran Shah <taran.shah9@gmail.com>
2024-05-01 23:10:10 +0530

    mongo

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect('process.env.MONGO_URI', {
+    .connect('mongodb+srv://taranshah9:Riddhima26@cluster0.ttapv4w.mongodb.net/?retryWrites=true', {
       dbName: "RESERVATIONS",
     })
     .then(() => {
